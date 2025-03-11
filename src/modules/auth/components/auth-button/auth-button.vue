3818f58614818414c76843f522ae010e0f1c19f2
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-03-10 19:22:23 -0500

    ✨ (auth): auth button component

---
+++ src/modules/auth/components/auth-button/auth-button.vue
@@ -0,0 +1,26 @@
+<template>
+  <Button
+    variant="outline"
+    class="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500 rounded-full shadow-none"
+  >
+    <UserCircleIcon />
+    Sign In
+  </Button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Button } from '@/components/ui/button'
+import { UserCircleIcon } from 'lucide-vue-next'
+
+export default defineComponent({
+  components: {
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Button,
+    UserCircleIcon,
+  },
+  setup() {},
+})
+</script>
+
+<style scoped></style>
