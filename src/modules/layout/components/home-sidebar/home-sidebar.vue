a1f07ebc524380e72ec18a0a4f147f4442250b86
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2025-03-10 21:29:43 -0500

    ♻️ (layout): handle validations onchange openSidebar property

--- src/modules/layout/components/home-sidebar/home-sidebar.vue
+++ src/modules/layout/components/home-sidebar/home-sidebar.vue
@@ -1,13 +1,40 @@
 <template>
-  <div></div>
+  <aside class="sidebar " :class="{ '--is-open': stateSidebar }">
+    <main-section />
+  </aside>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, type PropType } from 'vue'
+import type { ILayoutView } from '../../view/layout-view.types';
+import type { ILayoutController } from '../../controller';
+import MainSection from './main-section/main-section.vue';
+
 
 export default defineComponent({
-  setup() {},
+  components: {
+    MainSection
+  },
+  props: {
+    layoutView: {
+      type: Object as PropType<ILayoutView>,
+      required: true
+    },
+    layoutController: {
+      type: Object as PropType<ILayoutController>,
+      required: true
+    }
+  },
+  setup() {
+  },
+  computed: {
+    stateSidebar(): boolean {
+      return this.$props.layoutView.openSidebar
+    }
+  }
 })
 </script>
 
-<style scoped></style>
+<style scoped>
+@import './home-sidebar.css';
+</style>
