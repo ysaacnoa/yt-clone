5246a65537a5ee5522064f3ce56dc62fd19ddb44
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2025-03-10 21:29:05 -0500

    ♻️ (layout): implement view and controler into layout and navbar

--- src/modules/layout/components/home-layout/home-layout.vue
+++ src/modules/layout/components/home-layout/home-layout.vue
@@ -1,24 +1,51 @@
 <template>
   <aside class="">
-    <home-navbar />
-
-    <slot/>
+    <SidebarProvider>
+      <home-navbar :layout-view="layoutView" :layout-controller="layoutController" />
+      <div class="flex min-h-screen pt-[4rem]">
+        <home-sidebar :layout-view="layoutView" :layout-controller="layoutController" />
+        <main class="main-container" :class="{ '--is-open': isOpen }">
+          <slot />
+        </main>
+      </div>
+    </SidebarProvider>
   </aside>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import HomeNavbar from '../home-navbar'
+import HomeSidebar from '../home-sidebar';
+import { LayoutView } from '../../view/layout-view';
+import { LayoutController, type ILayoutController } from '../../controller';
+import type { ILayoutView } from '../../view/layout-view.types';
+import SidebarProvider from '@/components/ui/sidebar/SidebarProvider.vue';
+
 export default defineComponent({
   components: {
-    HomeNavbar
+    HomeNavbar,
+    HomeSidebar,
+    SidebarProvider
   },
   setup() {
-
+    const layoutView = ref<ILayoutView>(new LayoutView());
+    const layoutController = ref<ILayoutController>(new LayoutController({
+      layoutView: layoutView.value
+    }))
+    console.debug({ layoutView, layoutController })
+    return {
+      layoutView,
+      layoutController
+    }
   },
+  computed: {
+    isOpen(): boolean {
+      return this.layoutView.openSidebar
+    }
+  }
 })
 </script>
 
 <style scoped>
-
+@import './home-layout.css';
 </style>
--- src/modules/layout/components/home-navbar/home-navbar.vue
+++ src/modules/layout/components/home-navbar/home-navbar.vue
@@ -1,7 +1,8 @@
 <template>
   <nav class="fixed h-[60px] top-0 left-0 px-4 w-full flex flex-row items-center">
     <div class="flex flex-row items-center">
-      <MenuIcon class="hover:cursor-pointer"/>
+
+      <MenuIcon class="hover:cursor-pointer" @click="toggle" />
       <div class="w-[70px]">
         <img src="../../../../../public/logo.svg" alt="logo" loading="lazy" />
       </div>
@@ -17,10 +18,12 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, type PropType } from 'vue'
 import SearchInput from './search-input'
 import AuthButton from '@/modules/auth/components/auth-button'
 import { MenuIcon } from 'lucide-vue-next'
+import type { ILayoutView } from '../../view/layout-view.types'
+import type { ILayoutController } from '../../controller'
 
 export default defineComponent({
   components: {
@@ -28,7 +31,24 @@ export default defineComponent({
     AuthButton,
     MenuIcon
   },
-  setup() {},
+  props: {
+    layoutView: {
+      type: Object as PropType<ILayoutView>,
+      required: true,
+    },
+    layoutController: {
+      type: Object as PropType<ILayoutController>,
+      required: true
+    }
+  },
+  setup(props) {
+    const toggle = () => {
+      props.layoutController.toggleSidebar()
+    }
+    return {
+      toggle
+    }
+  },
 })
 </script>
 <style scoped></style>
