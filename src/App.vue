e48c5c417111f23cd806dd98053e5acde1d1a30c
ysaacnoa <ysaacnoe.correa@unmsm.edu.pe>
2025-03-10 19:21:13 -0500

    🎉 (yt-app): star proyejct

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <home-layout/>
+  <main class="pt-16">
+    hola mundo
+  </main>
+</template>
+
+<script lang="ts">
+import HomeLayout from '@/modules/layout/components/home-layout'
+import { defineComponent } from 'vue'
+export default defineComponent({
+  components: {
+    HomeLayout
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
