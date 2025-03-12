a550503e63495a3a50e841e235346f2184b109df
LilAtza <ysaacnoe.correa@unmsm.edu.pe>
2025-03-12 02:04:58 -0500

    ✨ (auth): install and config clerck

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { clerkPlugin } from '@clerk/vue'
 
-createApp(App).mount('#app')
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+const app = createApp(App)
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY })
+app.mount('#app')
